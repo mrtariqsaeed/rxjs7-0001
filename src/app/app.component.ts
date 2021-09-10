7a37cd084850f5e637e076bee9002a932c500727
Tariq Saeed <eng.tariqsaeed@gmail.com>
2021-09-11 00:08:18 +0200

    topromise deprecation practicing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { firstValueFrom, lastValueFrom, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'practice';
+  todosURL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+  constructor(private http: HttpClient) {
+    this.getTodos()
+  }
+
+  async getTodos() {
+    // const data$ = this.http.get(this.todosURL)
+    const data$ = new Observable(subscriber => {
+      subscriber.next('Todo 1')
+      subscriber.complete()
+    })
+    
+    try {
+      const value = await lastValueFrom(data$, {defaultValue: 'Default'}) ?? 'Default 2'
+      console.log('value ->', value)
+    } catch(e) {
+      console.log(e)
+    }
+    
+  }
 }
